ccae190989db3e2971eb6c0eb9300823e63c72ad
xzming <xzm@umshz.com>
2019-08-16 16:05:42 +0800

    reformat code

--- cutapple.py
+++ cutapple.py
@@ -1,74 +1,77 @@
-# use labelImg to label VOC Format, then use this to cut target object. 
+# use labelImg to label VOC Format, then use this to cut target object.
 
-import cv2 
+import cv2
 import numpy as np
 import os
 
 import xml.etree.ElementTree as Et
 from xml.etree.ElementTree import Element, ElementTree
 
+
 def parseAndGenerateImg():
-	path = os.getcwd() + "/Labels"
-	print("curpath: ", path)
-	(dir_path, dir_names, filenames) = next(os.walk(os.path.abspath(path)))
+    path = os.getcwd() + "/Labels"
+    print("curpath: ", path)
+    (dir_path, dir_names, filenames) = next(os.walk(os.path.abspath(path)))
 
-	boxCount = 0
-	for filename in filenames:
-		xmlPath = os.path.join(dir_path, filename)
-		xml = open(xmlPath, "r")
-		tree = Et.parse(xml)
-		root = tree.getroot()
+    boxCount = 0
+    for filename in filenames:
+        xmlPath = os.path.join(dir_path, filename)
+        xml = open(xmlPath, "r")
+        tree = Et.parse(xml)
+        root = tree.getroot()
 
-		imagePath = root.find("path").text
+        imagePath = root.find("path").text
 
-		xml_size = root.find("size")
-		size = {
-			"width": xml_size.find("width").text,
-			"height": xml_size.find("height").text,
-			"depth": xml_size.find("depth").text
-		}
+        xml_size = root.find("size")
+        size = {
+            "width": xml_size.find("width").text,
+            "height": xml_size.find("height").text,
+            "depth": xml_size.find("depth").text
+        }
 
-		objects = root.findall("object")
-		if len(objects) == 0:
-			return False, "number object zero"
+        objects = root.findall("object")
+        if len(objects) == 0:
+            return False, "number object zero"
 
-		obj = {
-			"num_obj": len(objects)
-		}
+        obj = {
+            "num_obj": len(objects)
+        }
 
-		print("filename", imagePath)
+        print("filename", imagePath)
 
-		obj_index = 0
-		imageInfo = cv2.imread(imagePath)
-		for _object in objects:
-			tmp={
-	            "name": _object.find("name").text
-	        }
+        obj_index = 0
+        imageInfo = cv2.imread(imagePath)
+        for _object in objects:
+            tmp = {
+                "name": _object.find("name").text
+            }
 
-			xml_bndbox = _object.find("bndbox")
-			bndbox = {
-				"xmin": int(xml_bndbox.find("xmin").text),
-				"ymin": int(xml_bndbox.find("ymin").text),
-				"xmax": int(xml_bndbox.find("xmax").text),
-				"ymax": int(xml_bndbox.find("ymax").text)
-			}
-			tmp["bndbox"] = bndbox
-			obj[str(obj_index)] = tmp
-			
-			obj_index += 1
-			print("bndbox", bndbox)
+            xml_bndbox = _object.find("bndbox")
+            bndbox = {
+                "xmin": int(xml_bndbox.find("xmin").text),
+                "ymin": int(xml_bndbox.find("ymin").text),
+                "xmax": int(xml_bndbox.find("xmax").text),
+                "ymax": int(xml_bndbox.find("ymax").text)
+            }
+            tmp["bndbox"] = bndbox
+            obj[str(obj_index)] = tmp
 
-			imgBox = imageInfo[bndbox["ymin"]:bndbox["ymax"], bndbox["xmin"]:bndbox["xmax"]]
-			if not os.path.exists('objects/scale'):
-				os.makedirs('objects/scale')
+            obj_index += 1
+            print("bndbox", bndbox)
 
-			cv2.imwrite("objects/"+filename+str(obj_index)+".jpg", imgBox)
-	
-			resized_image = cv2.resize(imgBox, (50, 50))
-			cv2.imwrite("objects/scale/object"+str(boxCount)+".jpg", resized_image)
+            imgBox = imageInfo[bndbox["ymin"]
+                :bndbox["ymax"], bndbox["xmin"]:bndbox["xmax"]]
+            if not os.path.exists('objects/scale'):
+                os.makedirs('objects/scale')
 
-			boxCount += 1
+            cv2.imwrite("objects/" + filename +
+                        str(obj_index) + ".jpg", imgBox)
 
+            resized_image = cv2.resize(imgBox, (50, 50))
+            cv2.imwrite("objects/scale/object" +
+                        str(boxCount) + ".jpg", resized_image)
+
+            boxCount += 1
 
 
 parseAndGenerateImg()
--- download_imgs.py
+++ download_imgs.py
@@ -1,49 +1,52 @@
 # Download negative/background images.
 
-import 	cv2 
-import 	urllib.request
-import 	numpy as np
-import	os
+import cv2
+import urllib.request
+import numpy as np
+import os
 
 
 def scale_raw_images():
-	for rawimg in os.listdir('neg'):
-		try:
-			if os.path.isfile("neg/"+rawimg):
-				img = cv2.imread("neg/" + rawimg)
-				resized_image = cv2.resize(img, (100, 100))
-				cv2.imwrite("neg/scale_100/"+rawimg, resized_image)
-		except Exception as e:
-			print(str(e))
+    for rawimg in os.listdir('neg'):
+        try:
+            if os.path.isfile("neg/" + rawimg):
+                img = cv2.imread("neg/" + rawimg)
+                resized_image = cv2.resize(img, (100, 100))
+                cv2.imwrite("neg/scale_100/" + rawimg, resized_image)
+        except Exception as e:
+            print(str(e))
 
-	pass
+    pass
+
 
 def store_raw_images():
 
-	#     neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152'   
-    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'   
+    #     neg_images_link = '//image-net.org/api/text/imagenet.synset.geturls?wnid=n07942152'
+    neg_images_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
     neg_image_urls = urllib.request.urlopen(neg_images_link).read().decode()
     pic_num = 1
-    
+
     if not os.path.exists('neg/scale'):
         os.makedirs('neg/scale')
-        
+
     for i in neg_image_urls.split('\n'):
         try:
             print(i)
-            urllib.request.urlretrieve(i, "neg/"+str(pic_num)+".jpg")
-            img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
+            urllib.request.urlretrieve(i, "neg/" + str(pic_num) + ".jpg")
+            img = cv2.imread("neg/" + str(pic_num) +
+                             ".jpg", cv2.IMREAD_GRAYSCALE)
             # should be larger than samples / pos pic (so we can place our image on it)
             resized_image = cv2.resize(img, (100, 100))
             if not os.path.exists('neg/scale_100'):
-            	os.makedirs('neg/scale_100')
+                os.makedirs('neg/scale_100')
 
-            cv2.imwrite("neg/scale_100/"+str(pic_num)+".jpg",resized_image)
+            cv2.imwrite("neg/scale_100/" + str(pic_num) +
+                        ".jpg", resized_image)
 
             pic_num += 1
-            
+
         except Exception as e:
-            print(str(e)) 
+            print(str(e))
 
 
 def find_uglies():
@@ -52,10 +55,10 @@ def find_uglies():
         for img in os.listdir(file_type):
             for ugly in os.listdir('uglies'):
                 try:
-                    current_image_path = str(file_type)+'/'+str(img)
-                    ugly = cv2.imread('uglies/'+str(ugly))
+                    current_image_path = str(file_type) + '/' + str(img)
+                    ugly = cv2.imread('uglies/' + str(ugly))
                     question = cv2.imread(current_image_path)
-                    if ugly.shape == question.shape and not(np.bitwise_xor(ugly,question).any()):
+                    if ugly.shape == question.shape and not(np.bitwise_xor(ugly, question).any()):
                         print('That is one ugly pic! Deleting!')
                         print(current_image_path)
                         os.remove(current_image_path)
@@ -65,21 +68,19 @@ def find_uglies():
 
 def create_pos_n_neg():
     for file_type in ['neg/scale_100']:
-        
+
         for img in os.listdir(file_type):
 
             if file_type == 'pos':
-                line = file_type+'/'+img+' 1 0 0 50 50\n'
-                with open('info.dat','a') as f:
+                line = file_type + '/' + img + ' 1 0 0 50 50\n'
+                with open('info.dat', 'a') as f:
                     f.write(line)
             elif file_type == 'neg/scale_100':
-                line = file_type+'/'+img+'\n'
-                with open('bg.txt','a') as f:
+                line = file_type + '/' + img + '\n'
+                with open('bg.txt', 'a') as f:
                     f.write(line)
-
 
 
 store_raw_images()
-create_pos_n_neg()
+# create_pos_n_neg()
 # scale_raw_images()
-
